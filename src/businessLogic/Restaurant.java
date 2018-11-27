c63a1f3de79117afd217cdd7acd11acacc93e79d
nicolas <nicolas@192.168.1.134>
2018-11-27 12:19:08 +0100

    Fist class relation proposal

--- src/businessLogic/Restaurant.java
+++ src/businessLogic/Restaurant.java
@@ -1,14 +1,55 @@
 package businessLogic;
 
+import java.util.ArrayList;
+
 public class Restaurant {
 
+	private ArrayList<Table> smallTables;
+	private ArrayList<Table> mediumTables;
+	private ArrayList<Table> largeTables;
+	private ArrayList<Table> extraLargeTables;
+	
+	
 	public Restaurant() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
+
+	public ArrayList<Table> getSmallTables() {
+		return smallTables;
+	}
+
+	public void setSmallTables(ArrayList<Table> smallTables) {
+		this.smallTables = smallTables;
+	}
+
+	public ArrayList<Table> getMediumTables() {
+		return mediumTables;
+	}
+
+	public void setMediumTables(ArrayList<Table> mediumTables) {
+		this.mediumTables = mediumTables;
+	}
+
+	public ArrayList<Table> getLargeTables() {
+		return largeTables;
+	}
+
+	public void setLargeTables(ArrayList<Table> largeTables) {
+		this.largeTables = largeTables;
+	}
+
+	public ArrayList<Table> getExtraLargeTables() {
+		return extraLargeTables;
+	}
+
+	public void setExtraLargeTables(ArrayList<Table> extraLargeTables) {
+		this.extraLargeTables = extraLargeTables;
+	}
+	
 
 }
--- src/businessLogic/WaitingList.java
+++ src/businessLogic/WaitingList.java
@@ -1,7 +1,14 @@
 package businessLogic;
 
+import java.util.LinkedList;
+
 public class WaitingList {
 
+	LinkedList<Customer> customerQueueSmall = new LinkedList<Customer>();
+	LinkedList<Customer> customerQueueMedium = new LinkedList<Customer>();
+	LinkedList<Customer> customerQueueLarge = new LinkedList<Customer>();
+	LinkedList<Customer> customerQueueExtraLarge = new LinkedList<Customer>();
+	
 	public WaitingList() {
 		// TODO Auto-generated constructor stub
 	}
